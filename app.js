3e4c246235f391dce4d2b7a7a5ebd225f8e417b3
matias-ac <matias.acastro1@gmail.com>
2022-08-09 21:35:08 -0300

    comenta codigo sin uso para evitar ejecucion hasta tanto se resuelva su eliminacion

--- app.js
+++ app.js
@@ -4,24 +4,24 @@ const menu = document.getElementById('menu');
 
 function ejecutarPrograma() {
 
-    saludarUsuario();
-    mostrarMenu();
-    const cantidad = solicitarCantidad();
-    console.log('La cantidad ingresada fue de: ' + cantidad);
+    // saludarUsuario();
+    // mostrarMenu();
+    // const cantidad = solicitarCantidad();
+    // console.log('La cantidad ingresada fue de: ' + cantidad);
 
-    solicitarSabores(cantidad);
-    mostrarPedidoEnConsola(pedidoUsuario);
+    // solicitarSabores(cantidad);
+    // mostrarPedidoEnConsola(pedidoUsuario);
 
-    const totalSinDescuentos = calcularTotal(cantidad);
-    console.log('El total sin descuentos es de: ' + totalSinDescuentos);
-    const totalSegunCantidad = calcularDescuentoCantidad(cantidad, totalSinDescuentos);
-    console.log('El total según la cantidad que lleva es: ' + totalSegunCantidad);
-    mostrarTotal(cantidad);
-    procesarPago(totalSegunCantidad);
+    // const totalSinDescuentos = calcularTotal(cantidad);
+    // console.log('El total sin descuentos es de: ' + totalSinDescuentos);
+    // const totalSegunCantidad = calcularDescuentoCantidad(cantidad, totalSinDescuentos);
+    // console.log('El total según la cantidad que lleva es: ' + totalSegunCantidad);
+    // mostrarTotal(cantidad);
+    // procesarPago(totalSegunCantidad);
 
-    finalizarPedido();
-    resetearPedido();
+    // finalizarPedido();
+    // resetearPedido();
 }
 
-boton.onclick = ejecutarPrograma;
-menu.onclick = mostrarMenu;
+// boton.onclick = ejecutarPrograma;
+// menu.onclick = mostrarMenu;
