6618f955a9f6a027ec9d3fff0f61d0a272ad8ff4
Matias Ariel Castro <matias.acastro1@gmail.com>
2022-08-30 16:15:21 -0300

    utiliza async/await y fetch para generar el menu de empanadas

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,18 +1,36 @@
 const $menuEmpanadas = document.querySelector('#menu-contenedor');
+const DATA_LOCAL = './data/data.json';
 
 const generarMenuEmpanadas = (listaDeEmpanadas) => {
+
     listaDeEmpanadas.forEach(empanada => {
+
         const contenedor = document.createElement('div');
         const nombre = document.createElement('p');
         const descripcion = document.createElement('p');
     
-        nombre.textContent = empanada.nombre;
+        nombre.textContent = empanada.sabor;
         descripcion.textContent = `(${empanada.descripcion})`;
     
         contenedor.append(nombre, descripcion);
         $menuEmpanadas.appendChild(contenedor);
+
     });
 };
 
-generarMenuEmpanadas(listaSabores);
+const listaDeEmpanadas = [];
+
+const obtenerData = async () => {
+
+    const respuesta = await fetch(DATA_LOCAL);
+    const data = await respuesta.json();
+
+    data.empanadas.forEach(elemento => {
+        listaDeEmpanadas.push(elemento);
+    })
+
+    generarMenuEmpanadas(listaDeEmpanadas)
+};
+
+obtenerData();
 
